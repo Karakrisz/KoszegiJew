37126ed2c7830d30ad6bd6502346e70694021b97
Boaz <webevolution13@gmail.com>
2025-04-29 12:42:54 +0200

    upDate
    front end

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Applications/XAMPP/xamppfiles/htdocs/KoszegiJew/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="hero">
+    <div class="hero__container">
+      <div class="hero__content">
+        <h1 class="hero__title">Közösség, emlékezet, hagyomány</h1>
+        <p class="hero__description">
+          A Kőszegi Zsidó Hitközség a múlt tisztelete és a jövő építése jegyében
+          működik. Közösségünk célja, hogy megőrizze és bemutassa a kőszegi
+          zsidóság gazdag örökségét, valamint nyitott és befogadó közösségi
+          teret biztosítson mindazok számára, akik érdeklődnek kultúránk,
+          vallásunk és történelmünk iránt. Legyen szó ünnepekről,
+          megemlékezésekről vagy közös programokról, mindenkit szeretettel
+          várunk, hogy együtt éljük meg hagyományainkat és erősítsük a közösségi
+          összetartozást.
+        </p>
+        <NuxtLink to="#" class="hero__button">Csatlakozz hozzánk</NuxtLink>
+      </div>
+      <div class="hero__opening-hours">
+        <div class="hero__hours-container">
+          <span class="hero__hours-label">Zsinagóga nyitvatartása:</span>
+          <span class="hero__hours-time">H: 14:00-18:00</span>
+          <span class="hero__hours-time">K-Sz: Zárva</span>
+          <span class="hero__hours-time">Cs-V: 14:00-18:00</span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
